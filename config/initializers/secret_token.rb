fd97ab8850fc8b27f3a8458dbaa784d42fc4faff
Erin <zazainca@gmail.com>
2013-08-26 12:32:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LPMV::Application.config.secret_key_base = '2502063ae513db122fd1380934f554c530a15b08bf7d65e30ac7b8ab2fac39b420bee14c3c3a6d438a8012aa836a516ddeff1d396b65508f790663d623b79bc9'
